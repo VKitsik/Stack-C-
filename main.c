26214b7acb01bf43325626da93504462955cdb91
VKitsik <viktor.kitsik@mail.ru>
2022-12-29 07:00:31 +0700

    second commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N 8
+typedef int Data;
+typedef struct {
+    Data a[N];      // место для данных
+    unsigned int n; // сколько данных хранится
+} Stack;
+
+void print(Stack * s) {
+    for(unsigned int i = 0; i < s->n; i++)
+        printf("%d ", s->a[i]);
+    printf("\n");
+}
+
+int main() {
+    Stack stack = {{7, 4, 1}, 3};
+
+    print(&stack);
+    return 0;
+}
